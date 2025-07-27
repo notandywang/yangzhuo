d36d27b8111166398b8d803d3bed773ff5f721a8
Andy Wang <awang2003@hotmail.com>
2025-07-28 01:59:19 +0200

    ignore

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const image = document.getElementById("mainImage");
 
 let state = "firstframe";
+let clickDisabled = false;
 
 image.addEventListener("click", () => {
+  if (clickDisabled) return;
+
   if (state === "firstframe") {
     image.src = "assets/gifs/gif_letter.gif";
     state = "gif";
+    clickDisabled = true;
+    setTimeout(() => {
+      clickDisabled = false;
+    }, 8000); // Disable clicks for 8 seconds
   } else if (state === "gif") {
     image.src = "assets/readtext.png";
     state = "text";
